392839d1ce99ade5d61d300653251f9f30327484
UsamaHameed <usaham@hotmail.com>
2022-04-11 20:53:04 +0500

    resolving merge conflicts

--- src/App.js
+++ src/App.js
@@ -5,13 +5,6 @@ import BasicMenu from './components/BasicMenu'
 import Header from './components/Header'
 import ComparisonCard from './components/ComparisonCard';
 import ProfessorScreen from './pages/ProfessorScreen'
-<<<<<<< HEAD
-import Admin_Report from './pages/Admin_Report';
-import NavbarComponentGuest from './components/NavbarComponentGuest.jsx'
-import CompareFinal from './pages/CompareFinal';
-import Landing from './pages/Landing';
-import Login from './pages/Login';
-=======
 import Landing from './pages/Landing';
 import Homepage from './pages/Homepage';
 import NavbarComponentGuest from './components/NavbarComponentGuest.jsx';
@@ -25,42 +18,14 @@ import Rate from './pages/Rate';
 import Search from '@mui/icons-material/Search';
 import Signup from './pages/Signup';
 import NavbarComponentAdmin from './components/NavbarComponentAdmin';
->>>>>>> 99667a1232dba0abfb20f8aeba5d32f8e96e3454
 // import Compare from './pages/Compare';
 function App() {
   return (
       // <Compare />
-      <>
-      {/* <Compare></Compare> */}
-      {/* <CompareFinal></CompareFinal> */}
-      {/* <Course></Course> */}
-      {/* <ForgotPassword></ForgotPassword> */}
-      {/* <Homepage></Homepage> */}
-      {/* <Instructor></Instructor> */}
-      {/* <Landing></Landing> */}
-      {/* <Pages></Pages> */}
-      {/* <NavbarComponentAdmin></NavbarComponentAdmin> */}
-      <Login></Login>
-      {/* <ProfessorScreen></ProfessorScreen> */}
-      {/* <Rate></Rate> */}
-      {/* <Search></Search> */}
-      {/* <Signup></Signup> */}
-    {/* <BasicMenu></BasicMenu> */}
-    {/* <Landing></Landing> */}
-    {/* <ErrorPage></ErrorPage> */}
-    {/* <Homepage></Homepage> */}
-    {/* <NavbarComponentGuest></NavbarComponentGuest> */}
-    {/* <Search></Search> */}
-    {/* <Header></Header> */}
-    {/* <ResponsiveAppBar/> */}
-    {/* <ComparisonCard></ComparisonCard> */}
-    {/* <Admin_Report></Admin_Report> */}
-    {/* <NavbarComponentGuest></NavbarComponentGuest> */}
-    {/* <Landing></Landing> */}
-    {/* <Login></Login> */}
+    <>
 
-    {/* <ProfessorScreen></ProfessorScreen> */}
-    {/* <CompareFinal></CompareFinal> */}
+      <div>Checking</div>
+      
     </>
   );
 }
