abb84b37108d34700fcb024254588d518fba1233
UsamaHameed <usaham@hotmail.com>
2022-04-11 20:56:09 +0500

    merge conflict

--- src/components/ComparisonCard.jsx
+++ src/components/ComparisonCard.jsx
@@ -6,7 +6,6 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { Divider, Grid } from '@mui/material';
-import CardHeader from '@mui/material/CardHeader';
 import Rating from '@mui/material/Rating';
 import styles from '../pages/styles.module.css'
 
