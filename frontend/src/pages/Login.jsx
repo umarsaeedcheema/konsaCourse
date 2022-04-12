7c798c789249fc0db5d1c886dc2e82f1e711277e
UsamaHameed <usaham@hotmail.com>
2022-04-12 23:37:38 +0500

    minor changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,81 +1,15 @@
 import styles from "./styles.module.css";
 import { useState } from "react";
-// import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-// import IconButton from "@material-ui/core/IconButton";
-// import Visibility from "@material-ui/icons/Visibility";
-// import VisibilityOff from "@material-ui/icons/VisibilityOff";
-// import InputAdornment from "@material-ui/core/InputAdornment";
+import axios from "axios"
 
-// const Login = () => {
-// 	const [data, setData] = useState({ email: "", password: "" });
-// 	const [error, setError] = useState("");
 
-// 	const handleChange = ({ currentTarget: input }) => {
-// 		setData({ ...data, [input.name]: input.value });
-// 	};
+//TODO
+//1)	On pressing sign in, verify from db via api call and go to homepage. Update state (how ?)
+//2)	If doesn't exist, show error message
+//3)	Verify that its a lums email
 
-// 	const handleSubmit = async (e) => {
-// 		e.preventDefault();
-//         console.log(e.target.value)
-// 		// try {
-// 		// 	const url = "http://localhost:8080/api/auth";
-// 		// 	const { data: res } = await axios.post(url, data);
-// 		// 	localStorage.setItem("token", res.data);
-// 		// 	window.location = "/";
-// 		// } catch (error) {
-// 		// 	if (
-// 		// 		error.response &&
-// 		// 		error.response.status >= 400 &&
-// 		// 		error.response.status <= 500
-// 		// 	) {
-// 		// 		setError(error.response.data.message);
-// 		// 	}
-// 		// }
-// 	};
-
-// 	return (
-// 		<div className={styles.login_container}>
-// 			<div className={styles.login_form_container}>
-// 				<div className={styles.left}>
-// 					<form className={styles.form_container} onSubmit={handleSubmit}>
-// 						<h1>Login to Your Account</h1>
-// 						<input
-// 							type="email"
-// 							placeholder="Email"
-// 							name="email"
-// 							onChange={handleChange}
-// 							value={data.email}
-// 							required
-// 							className={styles.input}
-// 						/>
-// 						<input
-// 							type="password"
-// 							placeholder="Password"
-// 							name="password"
-// 							onChange={handleChange}
-// 							value={data.password}
-// 							required
-// 							className={styles.input}
-// 						/>
-// 						{error && <div className={styles.error_msg}>{error}</div>}
-// 						<button type="submit" className={styles.green_btn}>
-// 							Sing In
-// 						</button>
-// 					</form>
-// 				</div>
-// 				<div className={styles.right}>
-// 					<h1>New Here ?</h1>
-// 					<Link to="/signup">
-// 						<button type="button" className={styles.white_btn}>
-// 							Sing Up
-// 						</button>
-// 					</Link>
-// 				</div>
-// 			</div>
-// 		</div>
-// 	);
-// };
+//const signin = require ('../../../backend/')
 
 const Login = () => {
 	const [data, setData] = useState({ email: "", password: "" });
@@ -92,30 +26,14 @@ const Login = () => {
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-		// console.log(e.target.value)
-		// console.log(data.email)
-		// console.log(data.password)
-		// try {
-		// 	const url = "http://localhost:8080/api/auth";
-		// 	const { data: res } = await axios.post(url, data);
-		// 	localStorage.setItem("token", res.data);
-		// 	window.location = "/";
-		// } catch (error) {
-		// 	if (
-		// 		error.response &&
-		// 		error.response.status >= 400 &&
-		// 		error.response.status <= 500
-		// 	) {
-		// 		setError(error.response.data.message);
-		// 	}
-		// }
+		console.log("HELLO");
+		
 	};
 
 
 	let navigate = useNavigate()
 	return (
-		// <div className={styles.login_container}>
-		//{/* // <div> */}
+		
 		< div className={styles.login_form_container} >
 			<div className={styles.left}>
 				<h1 >New Here ?</h1>
@@ -136,14 +54,17 @@ const Login = () => {
 						navigate('/pages/landing')
 					}}>
 						KonsaCourse
+
 					</text>
-					{/* <text>KonsaCourse</text> */}
+
 				</div>
-				{/* </Link> className={styles.form_container}		 */}
+
 				<div className="d-flex align-items-center justify-content-center felx-column" style={{ flex: 9 }}>
-					{/* <div style={{alignContent:"center",height:"100%",width:"100%",justifyContent:"center",flexDirection:"column",borderTopLeftRadius:"10px",backgroundColor:"white"}}> */}
+
 					<form className={styles.form_container} onSubmit={handleSubmit}>
+
 						<h1 style={{ color: "black", fontSize: "120%" }}>Sign in to Your Account</h1>
+						
 						<input
 							type="email"
 							placeholder="Email"
@@ -161,22 +82,9 @@ const Login = () => {
 							value={data.password}
 							required
 							className={styles.input}
-						// endAdornment={
-						// 	<InputAdornment position="end" style={{color:"black",height:"10%"}}>
-						// 	  <IconButton
-						// 		onClick={handleClickShowPassword}
-						// 	  >
-						// 		{showPassword ? <Visibility /> : <VisibilityOff />}
-						// 	  </IconButton>
-						// 	</InputAdornment>
-						//   }
+						
 						/>
-						{/* <IconButton
-									onClick={handleClickShowPassword}
-								  >
-									{showPassword ? <Visibility /> : <VisibilityOff />}
-								  </IconButton> */}
-						{/* <button></button> */}
+						
 						{error && <div className={styles.error_msg}>{error}</div>}
 
 						<span className="d-flex justify-content-end"
@@ -188,18 +96,15 @@ const Login = () => {
 						</span>
 
 
-						<button type="submit"
-							className={styles.green_btn}
-						>
+						<button type="submit" className={styles.green_btn}>
 							Sign In
 						</button>
 
 					</form>
 				</div>
-				{/* </div> */}
 			</div>
+
 		</div >
-		//   </div>
 	)
 }
 
