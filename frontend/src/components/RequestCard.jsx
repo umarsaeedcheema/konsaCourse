a5ac74568445de54d4d07156edba8e688e0f7e2d
cheems <23100206@lums.edu.pk>
2022-04-14 00:46:19 +0500

    styled admin requests

--- frontend/src/components/RequestCard.jsx
+++ frontend/src/components/RequestCard.jsx
@@ -25,6 +25,12 @@ const RequestCard = (props) => {
                 <p>{props.profname}</p>
                 
             </CardContent>
+            <div
+            style={{
+                display:'flex',
+                justifyContent:'end'
+            }}
+            >
 
             <button type="button" className={styles.green_btn}>
 				Approve
@@ -33,6 +39,9 @@ const RequestCard = (props) => {
             <button type="button" className={styles.black_btn}>
 				Decline
 			</button>
+
+            </div>
+
 
 
 
--- frontend/src/pages/Admin_Requests.jsx
+++ frontend/src/pages/Admin_Requests.jsx
@@ -1,59 +1,82 @@
 import React from 'react'
 import RequestCard from '../components/RequestCard.jsx';
-import { Divider, Grid } from '@mui/material';
+import { autocompleteClasses, Divider, Grid } from '@mui/material';
 import Header from '../components/Header.jsx';
+import NavbarComponent from '../components/NavbarComponent.jsx';
+import styles from './styles.module.css'
+import { color } from '@mui/system';
+import { red } from '@mui/material/colors';
 
 
 
 const Admin_Requests = () => {
-  
-  const reviews = [["Sociology", "SOC 100", "Usama Hameed"],                 
+
+  const reviews = [["Sociology", "SOC 100", "Usama Hameed"],
   ["Computer Science", "CS 100", "Umar Cheema"],
   ["Artificial Intelligence", "CS 300", "Saifullah Khan"],
   ["Software Engineering", "CS 412", "Mustafa Arshad"]];
 
 
-  
-  
+
+
   return (
-    <>
-        <Grid container direction={'column'} spacing = {4}>
-            
-          {/* <Grid item container>   */}
-            
-            
+    <div style={{
+      width:'auto'
+    }} >
+      <div style={{
+        height:100, 
+      }}>
+        <NavbarComponent />
+      </div>
 
-            {reviews.map((value,key) => {
+      <h1
+      style={{
+        display:'flex',
+        justifyContent:'center'
+      }}
+      >
+        Request
+      </h1>
+      <Grid container direction={'column'} spacing={4}>
 
-              return (
-                <>
-                <Grid item container>  
-                  <Grid item xs={0} sm={2} />
-                  <Grid item xs={12} sm={8}>
+        {/* <Grid item container>   */}
 
-                    <RequestCard 
-                      coursename = {value[0]}
-                      coursecode = {value[1]}
-                      profname = {value[2]}
-                    />
 
-                  </Grid>
-                  <Grid item xs={0} sm={2} />
+
+        {reviews.map((value, key) => {
+
+          return (
+            <>
+
+
+
+              <Grid item container>
+                <Grid item xs={0} sm={2} />
+                <Grid item xs={12} sm={8}>
+
+                  <RequestCard
+                    coursename={value[0]}
+                    coursecode={value[1]}
+                    profname={value[2]}
+                  />
 
                 </Grid>
-                </>
-              )
+                <Grid item xs={0} sm={2} />
 
-            })}
+              </Grid>
+            </>
+          )
 
-            
+        })}
 
-          {/* </Grid> */}
-            
-        </Grid>
 
-        
-    </>
+
+        {/* </Grid> */}
+
+      </Grid>
+
+
+    </div>
   )
 }
 
