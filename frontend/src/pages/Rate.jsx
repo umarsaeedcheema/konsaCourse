9be088cc5ef4185607240b5218d1a125009192f5
UsamaHameed <usaham@hotmail.com>
2022-04-16 23:54:56 +0500

    Added search, fixed navbar

--- frontend/src/pages/Rate.jsx
+++ frontend/src/pages/Rate.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
 import styles from './styles.module.css'
 import NavbarComponent from '../components/NavbarComponent'
+import {useState,useEffect } from 'react'
+
+
 const Rate = () => {
+  
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("user");
+    if (loggedInUser) {
+      const foundUser = JSON.parse(loggedInUser);
+      setUser(foundUser);
+      console.log("Logged in User");
+    }
+  }, []);
+
+  let loggedin = false;
+  let adminin = false;
+  
+  if (user) {
+    if (user.isAdmin) {
+      adminin = true;
+    } else {
+      loggedin = true;
+    }
+  }
+  
+  
+  
   return (
     <div className={styles.gradient}>
         <NavbarComponent
-                isAdmin={false}
-                isLoggedIn={true}
+                isAdmin={adminin}
+                isLoggedIn={loggedin}
             />
 
 
